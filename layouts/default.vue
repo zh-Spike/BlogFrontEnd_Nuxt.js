1a80e3450d6eeb1c854bf5672545a867c6b0719b
ETNJL <etnjl@outlook.com>
2020-09-26 23:19:02 +0800

    Start default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,99 @@
 <template>
-  <div>
-    <Nuxt />
+  <div id="blog-box">
+    <div class="blog-header clear-fix">
+      <div class="logo-box float-left">
+        <div class="logo">博客</div>
+      </div>
+      <div class="login-tips-text-box float-right">
+        登录 | 注册
+      </div>
+      <div class="navigation-box float-right">
+        <span>首页</span>
+        <span>关于</span>
+        <span>友链</span>
+      </div>
+      <div class="user-info-box float-right" style="display: none">
+        用户信息
+      </div>
+    </div>
+    <Nuxt/>
+    <div class="blog-footer">
+      <div class="copy-right-box">
+        <p>
+          Copyright ©
+          <a href="http://localhost:8082/swagger-ui.html" target="_blank">
+            zh-spike
+          </a>
+          本网站由程序猿(媛)用爱驱动
+        </p>
+      </div>
+      <div>
+        <span>关于我们</span>
+        |
+        <span>联系我们</span>
+        |
+        <span>友情链接</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+.logo-box {
+  margin-left: 10px;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
+.logo-box .logo {
+  color: #737F90;
+  font-size: 24px;
+  font-weight: 600;
+}
+
+a {
+  text-decoration: none;
+}
+
+.copy-right-box {
+  margin-bottom: 10px;
+}
+
+.blog-footer {
+  color: #737F90;
+  padding-bottom: 30px;
+  text-align: center;
+}
+
+* {
+  padding: 0;
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+#blog-box {
+  width: 1140px;
+  margin: 0 auto;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.blog-header {
+  line-height: 30px;
+  margin-top: 20px;
+  background: #Fff;
+  padding: 10px;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+body {
+  background: #F2F2FA;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.float-left {
+  float: left;
+}
+
+.float-right {
+  float: right;
+}
+
+.clear-fix {
+  overflow: hidden;
+  zoom: 1;
 }
 </style>
